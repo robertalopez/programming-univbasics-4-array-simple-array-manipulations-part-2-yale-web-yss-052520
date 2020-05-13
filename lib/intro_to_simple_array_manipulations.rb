a4ae2bc6bd96b2231ed98a4d6b48f993e693a057
Robert Lopez <robert.lopez@yale.edu>
2020-05-13 22:01:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,6 @@
 def using_concat(array1, array2)
-  array1.concat(array2)
+ array1.concat(array2)
+  
 end
 
 def using_insert(array, element)
@@ -7,4 +8,5 @@ def using_insert(array, element)
 end 
 
 def using_uniq(array)
+  array.uniq
   
